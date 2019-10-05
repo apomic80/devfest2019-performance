c967754b477aac33f6c5ac6a6f751ae7197f8882
apomic80 <michele.aponte@blexin.com>
2019-10-05 06:59:36 +0200

    step1 done

---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+})
+export class ListaComponent implements OnInit {
+
+  text: string;
+  items: { label: string, value: number }[] = [];
+
+  addTextToList() {
+    const randomValue = Math.floor(Math.random() * 10) + 1;
+    this.items.push({ label: this.text, value: randomValue });
+  }
+
+  generateValue(value: number) {
+    console.log('value ' + value + ' generated');
+    return this.fibonacci(value * 4);
+  }
+
+  private fibonacci(num: number): number {
+    if (num === 1 || num === 2) {
+      return 1;
+    }
+    return this.fibonacci(num - 1) + this.fibonacci(num - 2);
+  }
+
+}
