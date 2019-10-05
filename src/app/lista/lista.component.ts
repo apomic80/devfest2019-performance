bd628bd02a91fb67f17f3070aad5bf1d0e92a002
apomic80 <michele.aponte@blexin.com>
2019-10-05 07:12:26 +0200

    step1 fix

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
 })
-export class ListaComponent implements OnInit {
+export class ListaComponent  {
 
   text: string;
   items: { label: string, value: number }[] = [];
